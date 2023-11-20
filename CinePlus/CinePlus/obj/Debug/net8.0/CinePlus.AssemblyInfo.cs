a03da4f96571dbc3e78629c388681950ba260d98
JordanZ <jordan.zelayahs@gmail.com>
2023-11-20 17:24:36 -0600

    Changed menu and added description

--- CinePlus/CinePlus/obj/Debug/net8.0/CinePlus.AssemblyInfo.cs
+++ CinePlus/CinePlus/obj/Debug/net8.0/CinePlus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinePlus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e7523917b72a45bba3f9055cfe29f2d284789fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinePlus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CinePlus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
