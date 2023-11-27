b55955fe35389705ffa6a3ac6b552d239514b107
JordanZ <jordan.zelayahs@gmail.com>
2023-11-27 17:42:45 -0600

    Adde app

---
+++ cinePlus/cinePlus/Models/UsuarioModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace cinePlus.Models
+{
+    public class UsuarioModel
+    {
+        // Propiedad Id
+        public int Id { get; set; }
+        [Required]
+
+        [DisplayName("User Name")]
+        // Propiedad Name
+        public string Name { get; set; }
+        [Required]
+
+        // Propiedad Email
+        public string Email { get; set; }
+
+    }
+}
