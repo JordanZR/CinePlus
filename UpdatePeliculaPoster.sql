6b9a7178569359c1a169d7945d7ce52a54a5ab0a
JordanZ <jordan.zelayahs@gmail.com>
2023-11-30 18:03:48 -0600

    Modified queries files and added links for images

---
+++ UpdatePeliculaPoster.sql
@@ -0,0 +1,33 @@
+use Cineplus
+
+SELECT * from Peliculas
+
+Update Peliculas set PosterImagen = 'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2022/01/titanic-2577593.jpg?tf=3840x' WHERE PeliculaID = 1
+
+Update Peliculas set PosterImagen = 'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2022/03/cadena-perpetua-2636499.jpg?tf=3840x' WHERE PeliculaID = 2
+
+Update Peliculas set PosterImagen = 'https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2020/07/hipertextual-que-origen-es-pelicula-mas-cautivadora-christopher-nolan-2020316385.jpg?fit=1600%2C1067&quality=50&strip=all&ssl=1' WHERE PeliculaID = 3
+
+Update Peliculas set PosterImagen = 'https://blog.enciclo.org/wp-content/uploads/2022/03/El-padrino-1972-1250x625.jpg' WHERE PeliculaID = 4
+
+Update Peliculas set PosterImagen = 'https://larepublica.cronosmedia.glr.pe/migration/images/TLBIGOIGTFGCBIU4TECGW7KOLA.jpg' WHERE PeliculaID = 5
+
+Update Peliculas set PosterImagen = 'https://cloudfront-eu-central-1.images.arcpublishing.com/prisaradio/FKHXZIYA6FNERPSK5O7NDFVAPI.jpg' WHERE PeliculaID = 6
+
+Update Peliculas set PosterImagen = 'https://cdn.aarp.net/content/dam/aarp/entertainment/movies-for-grownups/2022/03/1140-forrest-gump-bench-esp.jpg' WHERE PeliculaID = 7
+
+Update Peliculas set PosterImagen = 'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2019/06/pelicula-matrix-cumple-20-anos-volvera-cines-espana.jpg?tf=3840x' WHERE PeliculaID = 8
+
+Update Peliculas set PosterImagen = 'https://i.blogs.es/535dc3/el-senor-de-los-anillos-y-el-hobbit/1366_2000.jpeg' WHERE PeliculaID = 9
+
+Update Peliculas set PosterImagen = 'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2016/09/pulp-fiction.jpg?tf=3840x' WHERE PeliculaID = 10
+
+Update Peliculas set PosterImagen = 'https://assets.mubicdn.net/images/film/87065/image-w1504_overlaid.jpg?1635149421' WHERE PeliculaID = 26
+
+Update Peliculas set PosterImagen = 'https://hips.hearstapps.com/hmg-prod/images/interstellar-final-explicado-1589395771.jpg?resize=2048:*' WHERE PeliculaID = 27
+
+Update Peliculas set PosterImagen = 'https://media.gq.com.mx/photos/5f6a3cb772a4a63a5ce11708/16:9/w_2560%2Cc_limit/Goodfellas-GQ3.jpg' WHERE PeliculaID = 28
+
+Update Peliculas set PosterImagen = 'https://c.files.bbci.co.uk/FA65/production/_123010146_fightclub_alamy.jpg' WHERE PeliculaID = 29
+
+Update Peliculas set PosterImagen = 'https://elcomercio.pe/resizer/-xKJXp_t-FUcRs1kQnx_0J0_0Do=/1200x900/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/4VJSJT5RDFEMZO3TMGEYIWBTHA.jpg' WHERE PeliculaID = 30
