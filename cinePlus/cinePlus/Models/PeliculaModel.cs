6b6190d55322ee55080a4658c43975063a392c07
JordanZ <jordan.zelayahs@gmail.com>
2023-12-01 13:34:32 -0600

    Added PeliculasModel

---
+++ cinePlus/cinePlus/Models/PeliculaModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace cinePlus.Models
+{
+    public class PeliculaModel
+    {
+        [Required]
+        public int PeliculaID { get; set; }
+        [Required]
+
+        public string PosterImagen { get; set; }
+        [Required]
+        public string Titulo { get; set; }
+        [Required]
+
+        public string Sinopsis { get; set; }
+        [Required]
+
+        public string Director { get; set; }
+        [Required]
+
+        public string Genero { get; set; }
+        [Required]
+
+        public int Puntos { get; set; }
+    }
+}
