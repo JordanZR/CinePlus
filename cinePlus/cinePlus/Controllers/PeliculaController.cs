376489272ff315950dc478f80acb98511f2ab570
JordanZ <jordan.zelayahs@gmail.com>
2023-12-01 13:53:59 -0600

    Added PeliculaController in front end

---
+++ cinePlus/cinePlus/Controllers/PeliculaController.cs
@@ -0,0 +1,31 @@
+﻿using cinePlus.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace cinePlus.Controllers
+{
+    public class PeliculaController : Controller
+    {
+        Uri baseAddress = new Uri("http://localhost:4000/peliculas");
+        private readonly HttpClient _httpClient;
+
+        public PeliculaController()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = baseAddress;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<PeliculaModel> listaPeliculas = new List<PeliculaModel>();
+            HttpResponseMessage response = _httpClient.GetAsync(_httpClient.BaseAddress).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                listaPeliculas = JsonConvert.DeserializeObject<List<PeliculaModel>>(data);
+            }
+            return View(listaPeliculas);
+        }
+    }
+}
